3d700e3e5cd9b077ebb96d0243c4670408c45db3
Luiz Calaça <luizcalaca@gmail.com>
2022-12-29 17:06:09 -0300

    feat: pages initial structured

---
+++ client/src/pages/Single.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Single = () => {
+    return (
+      <div>
+        Single
+      </div>
+    )
+  }
+  
+export default Single
---
+++ client/src/pages/Write.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Write = () => {
+    return (
+      <div>
+        Write
+      </div>
+    )
+  }
+  
+export default Write
