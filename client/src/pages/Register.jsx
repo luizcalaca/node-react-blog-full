abffbbcfe0e94a52dc10fa39e4f973bc317098c2
Luiz Calaça <luizcalaca@gmail.com>
2022-12-29 18:12:01 -0300

    feat: register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Register = () => {
-    return (
-      <div>
-        Register
-      </div>
-    )
+  const [err, setError] = useState(null);
+
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+        />
+        <button>Register</button>
+        {err && <p>{err}</p>}
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
   }
   
 export default Register
