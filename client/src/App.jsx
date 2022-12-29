793911e43e903968a736cf5d9835b5ac4b94e322
Luiz Calaça <luizcalaca@gmail.com>
2022-12-29 17:58:18 -0300

    refactor: adjustment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import Home from "./pages/Home";
 import Single from "./pages/Single";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import './style.scss'
+
 const Layout = () => {
   return (
     <>
