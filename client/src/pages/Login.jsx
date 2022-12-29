ed3b30efead89cbd5bb9b1e8f56c726f0f101c7e
Luiz Calaça <luizcalaca@gmail.com>
2022-12-29 17:57:43 -0300

    feat: creating form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
     return (
-      <div>
-        Login
+        <div className="auth">
+        <h1>Login</h1>
+        <form>
+          <input
+            required
+            type="text"
+            placeholder="username"
+            name="username"
+          />
+          <input
+            required
+            type="password"
+            placeholder="password"
+            name="password"
+          />
+          <button>Login</button>
+          <span>
+            Don't you have an account? <Link to="/register">Register</Link>
+          </span>
+        </form>
       </div>
     )
   }
