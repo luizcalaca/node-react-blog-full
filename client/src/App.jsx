d99ae177f82971237276c9fb70df152896db6855
Luiz Calaça <luizcalaca@gmail.com>
2022-12-29 17:18:00 -0300

    feat: routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,42 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/post/:id",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello World
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   )
 }
