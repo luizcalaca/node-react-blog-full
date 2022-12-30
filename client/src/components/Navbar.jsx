84d7d507fd759548ac6ac6e1e86710a91cd48352
Luiz Calaça <luizcalaca@gmail.com>
2022-12-29 21:54:30 -0300

    feat: building nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../img/logo.png";
 
 const Navbar = () => {
-    return (
-      <div>
-        Navbar
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+          <img src={Logo} alt="" />
+          </Link>
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
-    )
-  }
-  
+    </div>
+  );
+};
+
 export default Navbar
