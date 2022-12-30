0acc9d07dc7c7d3da233d70763786528f675e0d7
Luiz Calaça <luizcalaca@gmail.com>
2022-12-30 16:48:45 -0300

    delete: rename file

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -9,12 +9,12 @@ export const AuthContexProvider = ({ children }) => {
   );
 
   const login = async (inputs) => {
-    const res = await axios.post("/auth/login", inputs);
+    const res = await axios.post("http://localhost:8800/api/auth/login", inputs);
     setCurrentUser(res.data);
   };
 
   const logout = async (inputs) => {
-    await axios.post("/auth/logout");
+    await axios.post("http://localhost:8800/api/auth/logout");
     setCurrentUser(null);
   };
 
