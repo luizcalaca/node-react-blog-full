57363a4b22343d60881381511bb81b1ca5cb3ad0
Luiz Calaça <luizcalaca@gmail.com>
2022-12-30 16:35:12 -0300

    feat: adding context API into main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AuthContexProvider } from "./context/authContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthContexProvider>
+      <App />
+    </AuthContexProvider>
   </React.StrictMode>,
 )
