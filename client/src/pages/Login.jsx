4a3cfb31fb47121f7c1e0478e939058245b8d2ed
Luiz Calaça <luizcalaca@gmail.com>
2022-12-30 16:49:08 -0300

    refactor: adjustment

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -11,6 +12,9 @@ const Login = () => {
   const [err, setError] = useState(null);
 
   const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
+
 
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
